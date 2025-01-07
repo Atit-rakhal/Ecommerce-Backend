6b0679fbfe1bf3e24c1644b0b3ba36491bfab75b
atit <subarnasubedi11@gmail.com>
2025-01-07 21:56:20 +0545

    Error handeling updated

--- middlewares/errorhandler.js
+++ middlewares/errorhandler.js
@@ -1,16 +1,26 @@
-const notfound= (req,res,next)=>{
-const error= new Error(`Not found ${originalUrl}`)
-res.status(404);
-next(error);
+/* Handle error for page not found or requested url not found  */
+const notfound = (req, res, next) => {
+    const error = new Error(`The resource at ${req.originalUrl} was not found.`);
+    res.status(404);  
+    next(error); 
+}
 
-};
+/* general error handeling  */
 
-const errorHandler=(err,req,res,next)=>{
-    const statusCode= res.statusCode == 200?500:res.statusCode;
-    res.status(statusCode);
+const errorHandler = (err, req, res, next) => {
+   
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    res.status(statusCode);  
+
+    // Send a JSON response with error details
     res.json({
-        message:error?.message,
-        stack: err?.stack,
+        message: statusCode === 404 
+            ? "Sorry, the page you requested could not be found."  
+            : "An unexpected error occurred. Please try again later.",  
+        stack: process.env.NODE_ENV === 'development' ? err?.stack : undefined,  // Include stack trace only in development
     });
 };
+
+
+
  module.exports={notfound,errorHandler};
