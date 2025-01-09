2ab0699d7575e8fe4821e90035f0f13777af0c95
atit <subarnasubedi11@gmail.com>
2025-01-09 19:42:24 +0545

    hashing password using bcrypt

--- controllers/userCtrlr.js
+++ controllers/userCtrlr.js
@@ -1,32 +1,24 @@
+const expressAsyncHandler = require('express-async-handler');
 const User = require("../models/userModels");
-exports.createUser = async (req, res) => {
-    try {
-      console.log("User model:", User);
-  
-      const { email } = req.body;
-      console.log("Incoming data:", req.body);
-  
-      if (!email) {
+
+exports.createUser = expressAsyncHandler(async (req, res) => {
+    console.log("User model:", User);
+
+    const { email } = req.body;
+    console.log("Incoming data:", req.body);
+
+    if (!email) {
         return res.status(400).json({ message: "Email is required" });
-      }
-  
-      const findUser = await User.findOne({ email });
-      console.log("User found:", findUser);
-  
-      if (!findUser) {
+    }
+
+    const findUser = await User.findOne({ email });
+    //it will find null if the email doesnt match 
+    console.log("User found:", findUser);
+
+    if (!findUser) {
         const newUser = await User.create(req.body);
         return res.status(201).json({ newUser });
-      } else {
+    } else {
         return res.status(400).json({ message: "Email already exists" });
-      }
-    } catch (error) {
-      console.error("Error in createUser:", error);
-  
-      if (error.name === "ValidationError") {
-        return res.status(400).json({ message: error.message });
-      }
-  
-      res.status(500).json({ message: "Internal server error", error: error.message });
     }
-  };
-  
+});
--- models/userModels.js
+++ models/userModels.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose'); // Erase if already required
+const bcrypt = require ('bcrypt')
 
 // Declare the Schema of the Mongo model
 var userSchema = new mongoose.Schema({
@@ -31,6 +32,14 @@ var userSchema = new mongoose.Schema({
     },
 });
 
+userSchema.pre("save",async function(next){
+    const salt= await bcrypt.genSaltSync(10);
+    this.password= await bcrypt.hash(this.password,salt)
+});
+
+userSchema.methods.isPasswordMatched = async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword,this.password);
+}
 //Export the model
 const User  = mongoose.model('User', userSchema);
 module.exports= User;
